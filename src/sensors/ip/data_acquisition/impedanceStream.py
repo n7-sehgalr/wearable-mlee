634204efdde81f990c51601e6ab736fd6afc9cb6
Rajat Sehgal <ngz4773@autuni.ac.nz>
2025-10-13 17:17:13 +1300

    pyqtgraph dynamic plotting

--- src/sensors/ip/data_acquisition/impedanceStream.py
+++ src/sensors/ip/data_acquisition/impedanceStream.py
@@ -1,10 +1,7 @@
-# Pyqtgraph native to Qt, uses QGraphicsScene. 
-# Therefore, more efficient than matplotlib
+from random import randint
 
 import pyqtgraph as pg
-from PyQt6 import QtWidgets
-
-# Create PlotWidget instance. All plots in pyqtgraph use this class
+from PyQt6 import QtCore, QtWidgets
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
@@ -30,13 +27,37 @@ class MainWindow(QtWidgets.QMainWindow):
         # Legend - use addLegend() method on PlotWidget object. 
         # Need to provide name for each line when calling plot()
         self.plot_graph.addLegend()
-
-        self.plot_graph.setXRange(1, 10)
+        self.plot_graph.showGrid(x=True, y=True)
         self.plot_graph.setYRange(20, 40)
         # Data
-        time = [1, 2, 3, 4, 5, 6, 7, 8 ,9, 10]
-        temperature = [30, 32, 34, 33, 32, 31, 35, 34, 32, 30]
-        self.plot_graph.plot(time, temperature, name="Temperature Sensor", pen=pen, symbol="o", symbolSize=15, symbolBrush="b")
+        self.time = list(range(10))
+        self.temperature = [randint(20, 40) for _ in range(10)]
+        
+        # Need a reference for line object to plot dynamically
+        self.line = self.plot_graph.plot(self.time, 
+                                         self.temperature,
+                                         name="Temperature Sensor", 
+                                         pen=pen, symbol="o", 
+                                         symbolSize=15, 
+                                         symbolBrush="b")
+        
+        # Add timer to simulate new temperature measurements
+        self.timer = QtCore.QTimer()
+        self.timer.setInterval(300)
+        self.timer.timeout.connect(self.update_plot)
+        self.timer.start()
+
+    def update_plot(self):
+        # Discard first value, then append 1 value higher at end
+        # Effectivly rolls time to left
+        self.time = self.time[1:]
+        self.time.append(self.time[-1] + 1)
+        # Next two lines roll temperature to left, adding random value at end
+        self.temperature = self.temperature[1:]
+        self.temperature.append(randint(20,40))
+        # Instead of random value, actual next value to come for IP. 
+        # For time, actual next timestamp
+        self.line.setData(self.time, self.temperature)
 
 app = QtWidgets.QApplication([])
 main = MainWindow()
